52eb3db732ee87e06cc17e41b2a18dafc7920a4c
dysonaii <dysonaii@github.com>
2026-09-02 14:17:27 +0800

    Initial commit: Add quick typing training project

---
+++ two_key_classification.py
@@ -0,0 +1,238 @@
+"""
+Boshiamy two-key character classification.
+Source: https://boshiamy.then.tw/two-keys + .cin cross-reference
+Categories: standard, headtail, shorthand, simplified, other, other2 (unclassified)
+"""
+
+two_key_classification = {
+    'A': {
+        'standard': '寸金也',
+        'headtail': '鎮會鑑威感救錢創戚錄之',
+        'shorthand': '俞華良成',
+        'simplified': '術',
+        'other': '鏡駐貪點澳族貸',
+        'other2': 'ぁあァア僉划犮矣鐵龍',
+    },
+    'B': {
+        'standard': '不半比人入',
+        'headtail': '資階為隱全陽隊院隆隔舒遵墜凝',
+        'shorthand': '並前曾義',
+        'simplified': '',
+        'other': '凍障善',
+        'other2': '敝舍',
+    },
+    'C': {
+        'standard': '夕中車巴央西尸史',
+        'headtail': '輿屬屠擊輻剝繫殿覆遲壁',
+        'shorthand': '',
+        'simplified': '',
+        'other': '溪宣履夠慰禍捷',
+        'other2': '乜盍舛虫襾！',
+    },
+    'D': {
+        'standard': '歹豆鬥斗大日刀力',
+        'headtail': '顯照裂影殘晚景題暴',
+        'shorthand': '既',
+        'simplified': '',
+        'other': '待得貿導渡召債',
+        'other2': '旦曰曷耑',
+    },
+    'E': {
+        'standard': '與丑山屯尹',
+        'headtail': '兩融習妥群辭釀彩舉劃崇頸段逼再正',
+        'shorthand': '夷司易',
+        'simplified': '靈',
+        'other': '峻',
+        'other2': 'ぇえへゑェエヘヱ丙免屮帚昜爪爰聿酉',
+    },
+    'F': {
+        'standard': '下上飛亡火曲戶方',
+        'headtail': '顧螢豐慧歐斷督匪榮爆疑',
+        'shorthand': '發',
+        'simplified': '',
+        'other': '營旗富業胡區',
+        'other2': '丰丱匕匚卌奏斿皿',
+    },
+    'G': {
+        'standard': '瓜千夾甘干女',
+        'headtail': '動怒裝重剩乘毀好',
+        'shorthand': '垂即就旁',
+        'simplified': '遷',
+        'other': '幾獎艦己媽教娛',
+        'other2': '乇喬幹爿臼',
+    },
+    'H': {
+        'standard': '心手',
+        'headtail': '積懊穩懷悔必懂稅慌頹悄惟黎稱',
+        'shorthand': '或巷很首愛',
+        'simplified': '',
+        'other': '程悅麵幫靜',
+        'other2': '乎恆禾者',
+    },
+    'I': {
+        'standard': '工欠言我',
+        'headtail': '讀鞏誤譯恐讓誨評說謊護誕講',
+        'shorthand': '應申甫',
+        'simplified': '',
+        'other': '跡該讚譴試掀',
+        'other2': 'ぃいゐィイヰ曳',
+    },
+    'J': {
+        'standard': '井',
+        'headtail': '戴載換撫幹惠控裁搶撤刺排栽搬擁挺挫擬',
+        'shorthand': '祭直是故',
+        'simplified': '',
+        'other': '擒損協',
+        'other2': '冓夬掐揀朿賁',
+    },
+    'K': {
+        'standard': '黃王斤',
+        'headtail': '質環氣理弄廷基',
+        'shorthand': '甚咸',
+        'simplified': '開長',
+        'other': '冠潛薦授懲徵靠賞趣兵養贊',
+        'other2': '冘壬尢幵戔旡气癸',
+    },
+    'L': {
+        'standard': '立辛來',
+        'headtail': '病勵熟廳哀彰痛競牽碟厲確麼壓礎',
+        'shorthand': '新親路夜',
+        'simplified': '麗廠',
+        'other': '磁疾',
+        'other2': '厂廣离耒襄離',
+    },
+    'M': {
+        'standard': '且門冊毛米目矛',
+        'headtail': '類矚盼關釋德往循驟閱縣衡睛從',
+        'shorthand': '買曼',
+        'simplified': '',
+        'other': '行邊',
+        'other2': 'んン悶衝釆闌',
+    },
+    'N': {
+        'standard': '角巾',
+        'headtail': '賽觸突寫安憲究勿危幅剛宗額竊逸塞定',
+        'shorthand': '求包象周敬',
+        'simplified': '',
+        'other': '市久',
+        'other2': 'んン匋寅竅',
+    },
+    'O': {
+        'standard': '亞口',
+        'headtail': '圓囑因距蹈園號跑圈戰唬圍嘲選',
+        'shorthand': '貫單嚴',
+        'simplified': '',
+        'other': '啊誓器圖品嚇跌',
+        'other2': '□ぉおをォオヲ僉咢噩囗',
+    },
+    'P': {
+        'standard': '卜至平皮片',
+        'headtail': '貨使候儘牌億譬依自側係停偶邀促',
+        'shorthand': '條個修',
+        'simplified': '',
+        'other': '卷歸泉',
+        'other2': '帛辟',
+    },
+    'Q': {
+        'standard': '里弓兼田丈',
+        'headtail': '弱獨體弧愚甲界獵獲遇',
+        'shorthand': '貴畢規',
+        'simplified': '',
+        'other': '默們繳蒙悲懼酬跟',
+        'other2': '卓',
+    },
+    'R': {
+        'standard': '臣耳儿乃爾',
+        'headtail': '共輩著聯舊聽落福視非藝萬遭',
+        'shorthand': '兆而考樂',
+        'simplified': '',
+        'other': '惹臨勒微',
+        'other2': '几廾廿散耐雚',
+    },
+    'S': {
+        'standard': '小生川牛世少身',
+        'headtail': '續輝縮黨總製省常緣覺綜繼堂縱',
+        'shorthand': '當',
+        'simplified': '',
+        'other': '績街淑線',
+        'other2': '卅敞糸賏？',
+    },
+    'T': {
+        'standard': '丁木',
+        'headtail': '檢樓想相可棒械標樣構楚',
+        'shorthand': '頁充弟更',
+        'simplified': '夢',
+        'other': '替臺碼面極哥模禁',
+        'other2': '云凸甬',
+    },
+    'U': {
+        'standard': '雨月予于永才又',
+        'headtail': '預勝熊脾態參台脫服遙墾',
+        'shorthand': '能',
+        'simplified': '',
+        'other': '遣粵鮮罪衡越腳',
+        'other2': 'ぅうゥウ凵凹於霍',
+    },
+    'V': {
+        'standard': '鬼佳弗万',
+        'headtail': '智無矮怎每複裡舞集鬆',
+        'shorthand': '覽詹敏',
+        'simplified': '',
+        'other': '賴副差表邁介、速礙',
+        'other2': '复奇復萬覆隹髟﹁﹂！',
+    },
+    'W': {
+        'standard': '瓦了午水巫文',
+        'headtail': '顏濁響濕濾漲海激灣測漂梁法滋洲',
+        'shorthand': '武谷',
+        'simplified': '',
+        'other': '溉流汽',
+        'other2': '么斌氶沸為韋',
+    },
+    'X': {
+        'standard': '升食牙氏',
+        'headtail': '殺在存',
+        'shorthand': '專鄉夸奄商',
+        'simplified': '',
+        'other': '鄰蜜藉密網諸徐儲蓄薄綠操贏隨',
+        'other2': '乂覃黽',
+    },
+    'Y': {
+        'standard': '士牙土',
+        'headtail': '都勢熱趕摯款壞起趁超境末願報遠州執',
+        'shorthand': '原幸厭憂',
+        'simplified': '聲',
+        'other': '嘉',
+        'other2': '丫坴夌',
+    },
+    'Z': {
+        'standard': '舟止竹子乙丸',
+        'headtail': '籲籍盤孤慮虧第戲劇歲築般遽',
+        'shorthand': '餐',
+        'simplified': '',
+        'other': '趨偵店處塑',
+        'other2': '孑孓虍豦',
+    },
+}
+
+CATEGORIES = [
+    ('standard', '來自標準字根'),
+    ('headtail', '來自頭尾碼'),
+    ('shorthand', '來自簡速字根'),
+    ('simplified', '來自簡體字'),
+    ('other', '其它'),
+    ('other2', '未分類'),
+]
+
+def get_category(char):
+    """Look up category of a character. Returns (letter, category) or None."""
+    for letter, cats in two_key_classification.items():
+        for category, chars in cats.items():
+            if char in chars:
+                return letter, category
+    return None
+
+def chars_by_category(letter):
+    """Return dict of category -> list of characters for a given letter."""
+    cats = two_key_classification.get(letter.upper(), {})
+    return {k: list(v) for k, v in cats.items() if v}
